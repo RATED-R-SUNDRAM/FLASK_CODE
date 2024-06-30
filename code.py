0460034c63a3ec392a722fd42e20fdaa45be2fd5
RATED-R-SUNDRAM <ss6437p@gmail.com>
2024-06-30 23:31:50 +0530

    added some code

--- code.py
+++ code.py
@@ -1,1 +1,14 @@
+#%%
 from flask import Flask, render_template, request, redirect, url_for
+# %%
+app = Flask(__name__)
+
+# %%
+@app.route('/')
+def index():
+    return "HI!!! BITCH"
+
+# %%
+if __name__ == '__main__':
+    app.run()
+# %%
